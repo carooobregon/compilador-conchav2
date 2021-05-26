bbefd541ac2df9042165e59011a8075df550a191
carooobregon <carooobregon@gmail.com>
2021-05-26 16:09:45 -0700

    added final goto

--- src/Utils/QuadReloaded.py
+++ src/Utils/QuadReloaded.py
@@ -25,14 +25,14 @@ class QuadReloaded:
             self.filaPrincipal.push(i)
     
     def lookUpMemoryVal(self, temp, const, var, val, scope):
-        if var.lookupVariableAddress(val, scope):
-            return var.lookupVariableAddress(val, scope)
-        elif const.lookupConstantAddress(val):
-            return const.lookupConstantAddress(val)
-        elif temp.lookupTempAddress(val):
-            return temp.lookupTempAddress(val)
-        elif self.symbolMemoryVal(val):
-            return self.symbolMemoryVal(val)
+        # if var.lookupVariableAddress(val, scope):
+        #     return var.lookupVariableAddress(val, scope)
+        # elif const.lookupConstantAddress(val):
+        #     return const.lookupConstantAddress(val)
+        # elif temp.lookupTempAddress(val):
+        #     return temp.lookupTempAddress(val)
+        # elif self.symbolMemoryVal(val):
+        #     return self.symbolMemoryVal(val)
         return val
 
     def symbolMemoryVal(self, sym):
@@ -124,3 +124,5 @@ class QuadReloaded:
     def currPrincipalCounter(self):
         return self.filaPrincipal.size()
     
+    def updateFirstGoto(self):
+        self.filaPrincipal.items[0][1] = self.filaPrincipal.size()+1
