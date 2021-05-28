f68375170f8ef7d5dc54efc1ed5ca2471786973f
carooobregon <carooobregon@gmail.com>
2021-05-27 17:02:28 -0700

    started performing jumps

--- src/VirtualMachine/virtualmachine.py
+++ src/VirtualMachine/virtualmachine.py
@@ -11,12 +11,31 @@ class VirtualMachine:
 		self.losQuads = []
 		pass
 
-	# def parseQuads(self):
+	def parseQuads(self):
+		print(self.losQuads)
+		startingPoint = self.losQuads[0][1]
+		cont = startingPoint-1
+		while (cont < len(self.losQuads)):
+			currQuad = self.losQuads[cont]
+			op = currQuad[0]
+			if op < 6:
+				self.handleOperations(currQuad)
+			elif op < 10:
+				self.handleTrueFalseOperations(currQuad)
+			elif op < 12:
+				cont = self.handleStackJumps(currQuad)
+				continue
+			elif op < 18:
+				self.handleFunctionOps(currQuad)
+			elif op < 19:
+				self.handleOtherOperations(currQuad)
+			cont+=1
 
 
 	def runVM(self):
 		self.handleFiles()
 		self.createGlobalScope()
+		self.parseQuads()
 
 	def createGlobalScope(self):
 		print(self.losQuads)
@@ -26,7 +45,6 @@ class VirtualMachine:
 			file = csv.reader(file)
 			for row in file:
 				cont = 0
-				print(row)
 				r = []
 				while(cont < len(row)):
 					curr = row[cont]
@@ -34,8 +52,9 @@ class VirtualMachine:
 					curr = curr.replace("]",'')
 					curr = curr.replace("''",'')
 					curr = curr.replace(" ",'')
-					cont+=1
-					print(curr)
+					if curr.isdigit():
+						curr = int(curr)
+					cont += 1
 					r.append(curr)
 				self.losQuads.append(r)
 		
@@ -49,80 +68,67 @@ class VirtualMachine:
 						row[cont] = int(row[cont])
 					cont+=1
 			
+	def handleOperations(self, q):
+		print("aqui ", q)
+		if(q[0] == 1):#sum
+			return q[1] + q[2]
 
-	def handleOperations(self):
-		for q in self.losQuads:
-			print(q)
-			# operands
-			if(q[0] == 1):#sum
-				return q[1] + q[2]
+		elif(q[0] == 2):#sub
+			return q[1] - q[2]
 
-			elif(q[0] == 2):#sub
-				return q[1] - q[2]
+		elif(q[0] == 3):#mul
+			return q[1] * q[2]
 
-			elif(q[0] == 3):#mul
-				return q[1] * q[2]
+		elif(q[0] == 4):#div
+			return q[1] / q[2]
 
-			elif(q[0] == 4):#div
-				return q[1] / q[2]
+		elif(q[0] == 5): # q[1] = q[2] asignacion
+			result =  q[2]
 
-	def handleTrueFalseOperations(self):
-		result = False
-		for q in self.losQuads:
-			print(q)
-			if(q[0] == 5): # q[1] = q[2] asignacion
-				result =  q[2]
+	def handleTrueFalseOperations(self,q):
+		if(q[0] == 6): # morethan
+			result = q[1] < q[2]
 
-			if(q[0] == 6): # morethan
-				result = q[1] < q[2]
+		elif(q[0] == 7): #lessthan
+			result = q[1] > q[2]
 
-			elif(q[0] == 7): #lessthan
-				result = q[1] > q[2]
+		elif(q[0] == 8): #notequal
+			result = q[1] != q[2]
 
-			elif(q[0] == 8): #notequal
-				result = q[1] != q[2]
+		elif(q[0] == 9):#equal
+			result = q[1] == q[2]
 
-			elif(q[0] == 9):#equal
-				result = q[1] == q[2]
-		return result
+	def handleStackJumps(self,q):
+		if(q[0] == 10):#goto
+			return q[1] - 1
 
-	def handleOtherOperations(self):
-		for q in self.losQuads:
-			print(q)
-			if(q[0] == 5): # q[1] = q[2] asignacion
-				result =  q[2]
-			elif(q[0] == 18):#return
-				print("ret")
+		elif(q[0] == 11):#gotof
+			print("gotof")
+			# if not access memory to look up self.losQuads[2] bool val 
+			# 	i = self.losQuads[1]
+			# else:
+			# 	continue
+			return q[1] - 1
+	
+	def handleFunctionOps(self,q):
+		if(q[0] == 12):#end
+			print("end")
 
-	def handleStackJumps(self):
-		i = 0
-		while i < len(self.losQuads):
-			print(self.losQuads[i])
-			if(self.losQuads[0] == 10):#goto
-				print("goto")
-				i = self.losQuads[1]
+		elif(q[0] == 13):#parameter
+			print("parm")
 
-			elif(self.losQuads[0] == 11):#gotof
-				print("gotof")
-				if not self.losQuads[2]:
-					i = self.losQuads[1]
-				else:
-					continue
+		elif(q[0] == 14):#write
+			print(self.losQuads[1])
 
-			elif(self.losQuads[0] == 12):#end
-				print("end")
+		elif(q[0] == 15):#endfunc
+			print("endfunc")
 
-			elif(self.losQuads[0] == 13):#parameter
-				print("parm")
+		elif(q[0] == 16):#era
+			print("era")
 
-			elif(self.losQuads[0] == 14):#write
-				print(self.losQuads[1])
+		elif(q[0] == 17):#gosub
+			print("gosub")
 
-			elif(self.losQuads[0] == 15):#endfunc
-				print("endfunc")
-
-			elif(self.losQuads[0] == 16):#era
-				print("era")
-
-			elif(self.losQuads[0] == 17):#gosub
-				print("gosub")
+	def handleOtherOperations(self,q):
+		if(q[0] == 18):#return
+			print("ret")
