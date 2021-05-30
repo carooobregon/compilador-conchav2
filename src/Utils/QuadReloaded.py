3fe820fb8978b69b960d0ae0d6ba43ac6b716868
carooobregon <carooobregon@gmail.com>
2021-05-30 08:26:07 -0500

    changed const in parse

--- src/Utils/QuadReloaded.py
+++ src/Utils/QuadReloaded.py
@@ -28,6 +28,7 @@ class QuadReloaded:
         if var.lookupVariableAddress(val, scope):
             return var.lookupVariableAddress(val, scope)
         elif const.lookupConstantAddress(val):
+            print("found", val)
             return const.lookupConstantAddress(val)
         elif temp.lookupTempAddress(val):
             return temp.lookupTempAddress(val)
